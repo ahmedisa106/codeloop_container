1ccec2ea02fcf5d85b848e28c3f5c2e644fcdb7c
abdohantash <abdohantash9999@gmail.com>
2022-05-26 11:42:52 +0200

    styles

---
+++ resources/views/admin/pages/companies/history.blade.php
@@ -0,0 +1,66 @@
+<div class="card">
+    <div class="card-header">
+        <h3>بيانات المؤسسة</h3>
+    </div>
+    <div class="card-body">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="table-responsive">
+                    <table class="table table-bordered datatable text-center">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>الباقة</th>
+                                <th>الحالة</th>
+                                <th>في</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>package 1</td>
+                                <td>مفعل</td>
+                                <td>20-3-2022</td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>package 2</td>
+                                <td>منتهي</td>
+                                <td>20-3-2022</td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>package 3</td>
+                                <td>منتهي</td>
+                                <td>20-3-2022</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="company-box">
+                    <img src="https://cdn.worldvectorlogo.com/logos/sa-logo-1.svg" alt="">
+                    <h4>Company 1</h4>
+                    <ul>
+                        <li>
+                            <p>الباقة الحالية</p>
+                            <span>package 1</span>
+                        </li>
+                        <li>
+                            <p>حالة الباقة</p>
+                            <span>مفعل</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="modal-footer">
+            <button class="btn btn-danger exsit_modal btn-air-danger btn-icon" type="button" data-bs-dismiss="modal">
+                <i class="fa fa-times"></i>
+                خروج
+            </button>
+        </div>
+    </div>
+</div>
--- resources/views/admin/pages/subscriptions/index.blade.php
+++ resources/views/admin/pages/subscriptions/index.blade.php
@@ -1,82 +1,95 @@
 @extends('admin.layouts.master')
 @push('title',$data['page_title'])
 @section('content')
-    <div class="card">
-        <div class="card-header">
-            <h3>{{$data['page_title']}}</h3>
-            <div class="btns-header">
-
-
-                <a href="{{route('subscriptions.create')}}" class="btn btn-primary show_modal btn-air-primary btn-icon">
-                    <i class="fa fa-plus"></i>
-                    {{$data['create']}}
-                </a>
-            </div>
+<div class="card">
+    <div class="card-header">
+        <h3>{{$data['page_title']}}</h3>
+        <div class="btns-header">
+            <a href="{{route('subscriptions.create')}}" class="btn btn-primary show_modal btn-air-primary btn-icon">
+                <i class="fa fa-plus"></i>
+                {{$data['create']}}
+            </a>
         </div>
-        <div class="card-body">
-            <div class="table-responsive">
+    </div>
+    <div class="card-body">
+        <form class="row" method="" action="">
+            <div class="col-md-6 form-group">
+                <label class="form-label">الحالة</label>
+                <select class="select2-custom">
+                    <option value="">مفعل</option>
+                    <option value="">منتظر التفعيل</option>
+                    <option value="">منتهي</option>
+                </select>
+            </div>
+            <div class="card-footer">
+                <button class="btn btn-primary btn-air-primary btn-icon" type="submit">
+                    <i class="fa fa-search"></i>
+                    بحث
+                </button>
+            </div>
+        </form>
+    </div>
+</div>
 
-                <table class="table table-bordered datatable text-center">
-                    <thead>
+<div class="card">
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-bordered datatable text-center">
+                <thead>
                     <tr>
-
-
                         <th>المؤسسه</th>
                         <th>الباقه</th>
                         <th>الحاله</th>
                         <th>التحكم</th>
                     </tr>
-                    </thead>
+                </thead>
+                <tbody>
 
 
-                    <tbody>
-
-
-                    </tbody>
-
-                </table>
-
-            </div>
+                </tbody>
+            </table>
         </div>
     </div>
+</div>
 
 @endsection
 @push('js')
-    <script>
+<script>
+    var table = $('.table.datatable').DataTable({
+        dom: 'lBfrtip',
+        lengthChange: true,
+        processing: true,
+        serverSide: true,
+        buttons: ['excel', 'pdf', 'print'],
+        ajax: {
+            url: '{{route('subscriptions.data')}}'
+        },
+        columns: [
 
-        var table = $('.table.datatable').DataTable({
-            dom: 'lBfrtip',
-            lengthChange: true,
-            processing: true,
-            serverSide: true,
-            buttons: ['excel', 'pdf', 'print'],
-            ajax: {
-                url: '{{route('subscriptions.data')}}'
+            {
+                name: 'company_id',
+                data: 'company_id'
             },
-            columns: [
-
-                {
-                    name: 'company_id', data: 'company_id'
-                },
-                {
-                    name: 'package_id', data: 'package_id'
-                },
-                {
-                    name: 'status', data: 'status'
-                },
-                {
-                    name: 'actions', data: 'actions'
-                },
+            {
+                name: 'package_id',
+                data: 'package_id'
+            },
+            {
+                name: 'status',
+                data: 'status'
+            },
+            {
+                name: 'actions',
+                data: 'actions'
+            },
 
 
-            ]
+        ]
 
-        });
+    });
 
-        table.buttons().container()
-            .appendTo('#DataTables_Table_0_wrapper .col-md-6:eq(0)');
+    table.buttons().container()
+        .appendTo('#DataTables_Table_0_wrapper .col-md-6:eq(0)');
 
-
-    </script>
+</script>
 @endpush
-
